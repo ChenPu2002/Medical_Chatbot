4caa460c95deb985c8a8ffe76ccd0b78a9eced61
Yiyi <yiyitang@connect.hku.hk>
2024-04-18 19:52:48 +0800

    Update .gitignore and api model

--- api_model.py
+++ api_model.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 openai.api_key = os.getenv("OPENAI_API_KEY")
+client = openai.OpenAI()
 
 def get_response(user_input, conversation_history, prompt_text):
     message = conversation_history + [{"role": "user", "content": user_input}]
@@ -14,10 +15,10 @@ def get_response(user_input, conversation_history, prompt_text):
         "temperature": 0.7,  # Lower temperature to reduce randomness
         "frequency_penalty": 0.8,  # Penalize new topics to focus on the current discussion
         "presence_penalty": 0.8,  # Encourage taking the lead in the conversation
-        "stop": ["\n", " Human:", " AI:"],  # Use stopping conditions to manage output
+        "stop": [" Human:", " AI:"],  # Use stopping conditions to manage output
     }
 
-    completion = openai.ChatCompletion.create(**prompt)
+    completion = client.chat.completions.create(**prompt)
     response = completion.choices[0].message.content
     conversation_history.append({"role": "assistant", "content": response})
     return response, conversation_history
