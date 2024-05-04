2e044b16e1feb1b3a96af3af1175097b8b43e386
ChenPu2002 <u3594723@connect.hku.hk>
2024-05-04 19:34:09 +0800

    update hint message

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-print("Loading libraries...")
+print("The first loading may be longer, for around 1 minute, but the following loading will be faster for less than 10 seconds.")
 from gui import ChatWindow as GUI
 from PyQt5.QtWidgets import QApplication
 import sys
-import time
 
 if __name__ == '__main__':
-    start_time = time.time()
 
     # code for running the GUI
     app = QApplication(sys.argv)
     app.setStyle('Fusion')  # Set the style to 'Fusion' for a modern look
     gui = GUI()
     gui.show()
-    print(f"GUI loaded in {time.time() - start_time:.2f} seconds")
 
     # click system exit to close the GUI
     sys.exit(app.exec_())
